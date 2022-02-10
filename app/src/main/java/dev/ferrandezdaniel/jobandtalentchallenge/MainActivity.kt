3a0f433e408510920497c58bb10f2f82adbb1bc8
Daniel Ferrandez Sanchez <d.ferrandez.sanchez@gmail.com>
2022-02-10 23:03:20 +0100

    Initial commit

---
+++ app/src/main/java/dev/ferrandezdaniel/jobandtalentchallenge/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.ferrandezdaniel.jobandtalentchallenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
