13b58b101630a99d46a3169c3b3276af38371f54
Daniel Ferrandez Sanchez <39792494+dferrandezsanchez@users.noreply.github.com>
2022-02-12 01:17:22 +0100

    feat: add api call on init viewmodel (#5)

--- app/src/main/java/dev/ferrandezdaniel/jobandtalentchallenge/di/RepositoryModule.kt
+++ app/src/main/java/dev/ferrandezdaniel/jobandtalentchallenge/di/RepositoryModule.kt
@@ -1,8 +1,9 @@
 package dev.ferrandezdaniel.jobandtalentchallenge.di
 
 import dev.ferrandezdaniel.data.features.restaurants.RestaurantsRepository
+import dev.ferrandezdaniel.domain.features.restaurants.IRestaurantsRepository
 import org.koin.dsl.module
 
 val repositoryModule = module {
-    single { RestaurantsRepository(get()) }
+    single<IRestaurantsRepository> { RestaurantsRepository(get()) }
 }
--- app/src/main/java/dev/ferrandezdaniel/jobandtalentchallenge/ui/features/list/ListFragment.kt
+++ app/src/main/java/dev/ferrandezdaniel/jobandtalentchallenge/ui/features/list/ListFragment.kt
@@ -1,7 +1,9 @@
 package dev.ferrandezdaniel.jobandtalentchallenge.ui.features.list
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import dev.ferrandezdaniel.jobandtalentchallenge.R
@@ -14,10 +16,17 @@ class ListFragment : Fragment(R.layout.list_fragment) {
     private val viewModel: ListViewModel by viewModel()
     private val binding: ListFragmentBinding by viewBinding()
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.listTitle.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_detailsFragment)
+        }
+        viewModel.restaurantsList.observe(viewLifecycleOwner) {
+            binding.listTitle.text = "Total Elementos= ${it.size}"
+        }
+        viewModel.restaurantsException.observe(viewLifecycleOwner) {
+            Toast.makeText(requireContext(), "ERROR= ${it.message}", Toast.LENGTH_SHORT).show()
         }
     }
 }
