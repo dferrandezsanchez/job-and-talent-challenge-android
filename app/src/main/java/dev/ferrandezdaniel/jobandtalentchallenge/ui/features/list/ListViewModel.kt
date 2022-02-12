4b04912b83bd4fd37cb1bd83f28e8c6cb4e81d5a
Daniel Ferrández <d.ferrandez.sanchez@gmail.com>
2022-02-12 01:16:40 +0100

    feat: add api call on init viewmodel

--- app/src/main/java/dev/ferrandezdaniel/jobandtalentchallenge/ui/features/list/ListViewModel.kt
+++ app/src/main/java/dev/ferrandezdaniel/jobandtalentchallenge/ui/features/list/ListViewModel.kt
@@ -1,7 +1,30 @@
 package dev.ferrandezdaniel.jobandtalentchallenge.ui.features.list
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dev.ferrandezdaniel.domain.core.models.restaurants.Restaurant
+import dev.ferrandezdaniel.domain.core.result.Response
+import dev.ferrandezdaniel.domain.features.restaurants.useCases.GetRestaurantsUseCase
+import kotlinx.coroutines.launch
 
-class ListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class ListViewModel(
+    private val getRestaurantsUseCase: GetRestaurantsUseCase
+) : ViewModel() {
+
+    private val _restaurantsList = MutableLiveData<List<Restaurant>>()
+    val restaurantsList: LiveData<List<Restaurant>> = _restaurantsList
+
+    private val _restaurantsException = MutableLiveData<Throwable>()
+    val restaurantsException: LiveData<Throwable> = _restaurantsException
+
+    init {
+        viewModelScope.launch {
+            when (val result = getRestaurantsUseCase()) {
+                is Response.Failure -> _restaurantsException.value = result.exception ?: Exception("MEEEEEHHH")
+                is Response.Success -> _restaurantsList.value = result.value ?: emptyList()
+            }
+        }
+    }
 }
