ebbb2ec2584fad292236e0ce52c87213e9fa1acb
Daniel Ferrandez Sanchez <39792494+dferrandezsanchez@users.noreply.github.com>
2022-02-14 07:54:50 +0100

    feat: add details fragment (#7)

--- app/src/main/java/dev/ferrandezdaniel/jobandtalentchallenge/ui/features/details/DetailsFragment.kt
+++ app/src/main/java/dev/ferrandezdaniel/jobandtalentchallenge/ui/features/details/DetailsFragment.kt
@@ -3,8 +3,8 @@ package dev.ferrandezdaniel.jobandtalentchallenge.ui.features.details
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import dev.ferrandezdaniel.jobandtalentchallenge.R
 import dev.ferrandezdaniel.jobandtalentchallenge.databinding.DetailsFragmentBinding
 import dev.ferrandezdaniel.jobandtalentchallenge.ui.core.bindings.viewBinding
@@ -18,9 +18,11 @@ class DetailsFragment : Fragment(R.layout.details_fragment) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.detailsTitle.text = arguments.restaurantModel.name
-        binding.detailsTitle.setOnClickListener {
-            findNavController().popBackStack()
+        with(arguments.restaurantModel) {
+            Glide.with(requireContext()).load(this.logo).into(binding.restaurantDetailsImage)
+            binding.restaurantDetailsTitle.text = this.name
+            binding.restaurantDetailsType.text = this.type
+            binding.restaurantDetailsDesc.text = this.description
         }
     }
 }
